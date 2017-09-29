3022f000a46753c2bc4566c3f6fa13800d29e680
Ezekiel Mugaya <emugaya@gmail.com>
2017-09-30 02:00:58 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
+import { FormsModule } from '@angular/forms';
+import { Http, HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import {
+  MatMenuModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatSlideToggleModule,
+  MatSelectModule,
+  MatInputModule,
+  MatButtonModule,
+  MatPaginatorModule
+ } from '@angular/material';
+
+ import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    BrowserModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
