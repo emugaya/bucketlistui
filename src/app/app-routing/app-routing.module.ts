08396bb40991f14a53d146f3885e3b27ee06f793
Ezekiel Mugaya <emugaya@gmail.com>
2017-09-30 03:28:58 +0300

    Updated Files with Login , Register components and underscore library

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,36 +1,33 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../services/auth-guard.service';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HomeComponent } from '../home/home.component';
-import { LoginComponent } from '../login/login.component';
-import { RegisterComponent } from '../register/register.component';
 import { BucketlistsComponent } from '../bucketlists/bucketlists.component';
 import { ItemsComponent } from '../items/items.component';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
 
 const routes: Routes = [
-	{ path: '', redirectTo: '/home', pathMatch: 'full' },
-	{ path: 'home', pathMatch: 'full', component: HomeComponent},
-	{ path: 'login',  component: LoginComponent},
-	{ path: 'register',  component: RegisterComponent},
-	{ path: 'bucketlists',
-	  component: BucketlistsComponent,
-	  canActivate: [AuthGuard]
-	},
-	{ path: 'bucketlist/:id', 
-	  component: ItemsComponent,
-	  canActivate: [AuthGuard]
-	},
-]
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'login',  component: LoginComponent},
+    { path: 'register',  component: RegisterComponent},
+    { path: 'bucketlists',
+    component: BucketlistsComponent,
+    canActivate: [AuthGuard]},
+    { path: 'bucketlist/:id',
+    component: ItemsComponent,
+    canActivate: [AuthGuard]
+},
+];
 
 @NgModule({
   imports: [
-		BrowserModule,
-  	RouterModule.forRoot(routes),
-    CommonModule
+      BrowserModule,
+      RouterModule.forRoot(routes),
+      CommonModule
   ],
   declarations: [],
   exports: [ RouterModule ]
